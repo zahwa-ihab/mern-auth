1583ea948060cd66c55d2ae259c27f9d7aa0473a
Zahwa <zahwa1703409@miuegypt.edu.eg>
2024-04-04 03:34:38 +0300

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGO)
+.then(
+    () => {
+        console.log('connected to mongoDB');
+    }
+).catch(
+    (err) => {
+        console.log(err);
+    }
+);
 const app= express();
+
 
 app.listen( 3000, () => { 
     console.log("Server Listening On Port 3000!!");
