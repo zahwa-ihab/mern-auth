a16b967aa0c5e44c879a6a2da6f1a011aaddb4d1
Zahwa <zahwa1703409@miuegypt.edu.eg>
2024-04-14 21:29:21 +0300

    signup page funcitonality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,35 +1,30 @@
 //request hena is the data we're taking from the user
 // while respose is the data we send back to client side
- import User from '../models/user.model.js';
- import bcryptjs from 'bcryptjs'; //encrypting pass before adding data to db
- import { errorHandler}  from '../utils/error.js';
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs"; //encrypting pass before adding data to db
+import { errorHandler } from "../utils/error.js";
 
-
-const signup = async (req , res , next) =>{
-    //next here is basically calling the middleware function
-    //this basically print all the data
-   console.log(req.body);
-   //but we want to destructure the data and save to our db
-   const { username, email, password} = req.body;
-   const hashedPassword = bcryptjs.hashSync(password,10);
-   const newUser = new User({username,email, password: hashedPassword});
-   // note that if the key and the value has the same name u don't have to use the passwprd : hashed
-   //saving new user to db takes time based on ur internet conn
-   // that why await here tells javascript to stay in that line till it's done
-  try{
-
-    await newUser.save()
-    res.status(201).json({message : "User created successully"}); 
-
-  }catch(error){
+const signup = async (req, res, next) => {
+  //next here is basically calling the middleware function
+  //this basically print all the data
+  //but we want to destructure the data and save to our db
+  const { username, email, password } = req.body;
+  console.log("Insie auth controller!");
+  console.log(req.body);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  // note that if the key and the value has the same name u don't have to use the passwprd : hashed
+  //saving new user to db takes time based on ur internet conn
+  // that why await here tells javascript to stay in that line till it's done
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "User created successully" });
+  } catch (error) {
     //caaling middleware
-     next(error);
-     //calling custom error
+    next(error);
+    //calling custom error
     //next(errorHandler(300,"error handler working"));
-
   }
-  
 };
-
 
 export default signup;
--- api/index.js
+++ api/index.js
@@ -1,43 +1,40 @@
-import express from 'express';
+import express from "express";
 ///we installed nodemon in order to listen automatically to any changes inside backend without having to restart
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';  
-import userRoutes from './routes/user.route.js'
-import authRoutes from './routes/auth.route.js'
-///dotenv allows us to access variables inside .env file 
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+///dotenv allows us to access variables inside .env file
 dotenv.config();
-mongoose.connect(process.env.MONGO)
-.then(
-    () => {
-        console.log('connected to mongoDB');
-    }
-).catch(
-    (err) => {
-        console.log(err);
-    }
-);
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to mongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-const app= express();
+const app = express();
 app.use(express.json());
 ///this allows json as the input of our backend
 ///note that we're using INSOMNIA to test our backend since UI IS NOT YET READY
 
-app.listen( 3000, () => { 
-    console.log("Server Listening On Port 3000!!");
+app.listen(3000, () => {
+  console.log("Server Listening On Port 3000");
 });
 
 //api route is an url that allows us to access api resources
-app.use( '/api/user' , userRoutes);
-app.use('/api/auth', authRoutes);
-
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 ///middleware
-app.use((err, req , res , next) => {
-    const statusCode = res.statusCode || 500;
-    const message = res.message || 'Internal Server Error!';
-    return res.status(statusCode).json({
-      success: false,
-      message,
-      statusCode
-    });
+app.use((err, req, res, next) => {
+  const statusCode = res.statusCode || 500;
+  const message = res.message || "Internal Server Error!";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode
+  });
 });
